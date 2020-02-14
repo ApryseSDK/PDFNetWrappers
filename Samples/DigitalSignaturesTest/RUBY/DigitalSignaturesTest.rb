a0c2931a9d79efbc0d7af452a9f010c1994570fd
mnguyen <mnguyen@pdftron.com>
2020-02-14 14:01:45 -0800

    update copyright 2020

--- Samples/DigitalSignaturesTest/RUBY/DigitalSignaturesTest.rb
+++ Samples/DigitalSignaturesTest/RUBY/DigitalSignaturesTest.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 
 #-----------------------------------------------------------------------------------------------------------------------
-# Copyright (c) 2001-2019 by PDFTron Systems Inc. All Rights Reserved.
+# Copyright (c) 2001-2020 by PDFTron Systems Inc. All Rights Reserved.
 # Consult LICENSE.txt regarding license information.
 #-----------------------------------------------------------------------------------------------------------------------
 
