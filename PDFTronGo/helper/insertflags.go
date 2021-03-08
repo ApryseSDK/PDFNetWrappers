d96ce0281c1ba9155f39a2747b6540c715061d37
bclkchen <kchen@pdftron.com>
2021-03-08 14:35:24 -0800

    added golang

---
+++ PDFTronGo/helper/insertflags.go
@@ -0,0 +1,30 @@
+//---------------------------------------------------------------------------------------
+// Copyright (c) 2001-2021 by PDFTron Systems Inc. All Rights Reserved.
+// Consult LICENSE.txt regarding license information.
+//---------------------------------------------------------------------------------------
+
+package main
+import (
+    "os"
+    "bytes"
+    "io/ioutil"
+)
+
+func main(){
+    //go run -x "./helper/insertflags.go" -- "../Build/PDFTronGo/pdftron.go" "#cgo CXXFLAGS: -I ../PDFNetC/Headers" "#cgo LDFLAGS: -L ../PDFNetC/Lib -lPDFNetC"
+    if(len(os.Args) < 4){
+        panic("The number of args is wrong!")
+    }
+    replacedString := "#define intgo swig_intgo"
+    newString := os.Args[3] + "\n" + os.Args[4] + "\n" + replacedString
+    content, err := ioutil.ReadFile(os.Args[2])
+    if err != nil {
+        panic(err)
+    }
+
+    lines := bytes.Replace(content, []byte(replacedString), []byte(newString), 1)
+    err = ioutil.WriteFile(os.Args[2], lines, 0644)
+    if err != nil {
+        panic(err)
+    }    
+}
