a57d4845aab8f97e88565c5f554b398c2fc41c21
Ken Chen <bclkchen@users.noreply.github.com>
2021-05-20 17:55:01 -0700

    Modified interface file

---
+++ Samples/LicenseKey/PYTHON/LicenseKey.py
@@ -0,0 +1,8 @@
+#---------------------------------------------------------------------------------------
+# Copyright (c) 2001-2021 by PDFTron Systems Inc. All Rights Reserved.
+# Consult LICENSE.txt regarding license information.
+#---------------------------------------------------------------------------------------
+
+LicenseKey = "YOUR_PDFTRON_LICENSE_KEY" #  Enter your license key here. Please go to https://www.pdftron.com/pws/get-key to obtain a demo key or https://www.pdftron.com/form/contact-sales to obtain a production key.
+if LicenseKey == "YOUR_PDFTRON_LICENSE_KEY":
+    raise Exception("Please go to https://www.pdftron.com/pws/get-key to obtain a demo key or https://www.pdftron.com/form/contact-sales to obtain a production key.")
