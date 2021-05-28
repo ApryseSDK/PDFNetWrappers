845c40eb529bb7f7a6366730afba04d30e5d4945
Ken Chen <bclkchen@users.noreply.github.com>
2021-05-27 20:30:15 -0700

    Moved comment to better place

--- Samples/LicenseKey/GO/LicenseKey.go
+++ Samples/LicenseKey/GO/LicenseKey.go
@@ -5,7 +5,8 @@
 
 package PDFTronLicense
 
-var Key = "YOUR_PDFTRON_LICENSE_KEY" // Enter your license key here. Please go to https://www.pdftron.com/pws/get-key to obtain a demo key or https://www.pdftron.com/form/contact-sales to obtain a production key.
+// Enter your license key here. Please go to https://www.pdftron.com/pws/get-key to obtain a demo key or https://www.pdftron.com/form/contact-sales to obtain a production key.
+var Key = "YOUR_PDFTRON_LICENSE_KEY"
 func init() {
 	if (Key == "YOUR_PDFTRON_LICENSE_KEY"){
 		panic("Please go to https://www.pdftron.com/pws/get-key to obtain a demo key or https://www.pdftron.com/form/contact-sales to obtain a production key.")
--- Samples/LicenseKey/PYTHON/LicenseKey.py
+++ Samples/LicenseKey/PYTHON/LicenseKey.py
@@ -3,6 +3,7 @@
 # Consult LICENSE.txt regarding license information.
 #---------------------------------------------------------------------------------------
 
-LicenseKey = "YOUR_PDFTRON_LICENSE_KEY" #  Enter your license key here. Please go to https://www.pdftron.com/pws/get-key to obtain a demo key or https://www.pdftron.com/form/contact-sales to obtain a production key.
+# Enter your license key here. Please go to https://www.pdftron.com/pws/get-key to obtain a demo key or https://www.pdftron.com/form/contact-sales to obtain a production key.
+LicenseKey = "YOUR_PDFTRON_LICENSE_KEY" 
 if LicenseKey == "YOUR_PDFTRON_LICENSE_KEY":
     raise Exception("Please go to https://www.pdftron.com/pws/get-key to obtain a demo key or https://www.pdftron.com/form/contact-sales to obtain a production key.")
