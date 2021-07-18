3c77e8d42dd155b1e4ee66122c9d5822d404b837
Ken Chen <bclkchen@users.noreply.github.com>
2021-07-18 09:00:38 -0700

    Added LicenseKey global variable.

--- Samples/AddImageTest/PHP/AddImageTest.php
+++ Samples/AddImageTest/PHP/AddImageTest.php
@@ -4,6 +4,7 @@
 // Consult LICENSE.txt regarding license information.
 //---------------------------------------------------------------------------------------
 include("../../../PDFNetC/Lib/PDFNetPHP.php");
+include("../../LicenseKey/PHP/LicenseKey.php");
 
 //-----------------------------------------------------------------------------------
 // This sample illustrates how to embed various raster image formats
@@ -13,7 +14,7 @@ include("../../../PDFNetC/Lib/PDFNetPHP.php");
 // be present in the system path.
 //-----------------------------------------------------------------------------------
 	
-	PDFNet::Initialize();
+	PDFNet::Initialize($LicenseKey);
 	PDFNet::GetSystemFontList();    // Wait for fonts to be loaded if they haven't already. This is done because PHP can run into errors when shutting down if font loading is still in progress.
 
 	// Relative path to the folder containing the test files.
