cdc9a50e047f822ba06fc330fbfbf69aac401194
Ken Chen <bclkchen@users.noreply.github.com>
2021-07-18 18:03:35 -0700

    Interface file is modified and class PDFTronLicense is added

--- Samples/AddImageTest/RUBY/AddImageTest.rb
+++ Samples/AddImageTest/RUBY/AddImageTest.rb
@@ -5,6 +5,7 @@
 
 require '../../../PDFNetC/Lib/PDFNetRuby'
 include PDFNetRuby
+require '../../LicenseKey/RUBY/LicenseKey'
 
 $stdout.sync = true
 
@@ -16,7 +17,7 @@ $stdout.sync = true
 # be present in the system path.
 #-----------------------------------------------------------------------------------
 
-	PDFNet.Initialize()
+	PDFNet.Initialize(PDFTronLicense.Key)
     
 	# Relative path to the folder containing test files.
 	input_path = "../../TestFiles/"
