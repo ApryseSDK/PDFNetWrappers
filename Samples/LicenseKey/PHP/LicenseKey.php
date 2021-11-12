907adb10792d2ee0bedd1c6bcf238e3635a360a5
Minh Nguyen (Mitch) <mnguyen@pdftron.com>
2021-11-12 12:35:46 -0800

    added ns to PHP (#38)

--- Samples/LicenseKey/PHP/LicenseKey.php
+++ Samples/LicenseKey/PHP/LicenseKey.php
@@ -3,6 +3,7 @@
 // Copyright (c) 2001-2021 by PDFTron Systems Inc. All Rights Reserved.
 // Consult LICENSE.txt regarding license information.
 //---------------------------------------------------------------------------------------
+namespace pdftron;
 
 //Enter your key here. If you don't have it, please go to https://www.pdftron.com/pws/get-key to obtain a demo license or https://www.pdftron.com/form/contact-sales to obtain a production key. 
 $LicenseKey = "YOUR_PDFTRON_LICENSE_KEY";
