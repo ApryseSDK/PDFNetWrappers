f87a8dfa8c57e4e49315aeaa858bc1e9bc3ac7ea
rachmatgithub <cilong@gmail.com>
2021-11-18 12:04:08 -0800

    Update replace.py

--- PDFTronGo/CI/Windows/replace.py
+++ PDFTronGo/CI/Windows/replace.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 def execute_replace(input, script):
    i = 0
@@ -32,40 +33,50 @@ def execute_replace(input, script):
          break
    return input
 
-with open("pdftron_wrap.cxx", "r") as f:
-   cxx = f.read()
+def replacego(filepath):
+    filepathname = os.path.join(filepath, "pdftron_wrap.cxx")
+	with open(filepathname, "r") as f:
+	   cxx = f.read()
 
-with open("pdftron_wrap.h", "r") as f:
-   h = f.read()
+    filepathname = os.path.join(filepath, "pdftron_wrap.h")
+	with open(filepathname, "r") as f:
+	   h = f.read()
 
-with open("pdftron.go", "r") as f:
-   go = f.read()
+    filepathname = os.path.join(filepath, "pdftron.go")
+	with open(filepathname, "r") as f:
+	   go = f.read()
 
-with open("pdftron_wrap.cxx.replace", "r") as f:
-   cxx_replace = f.readlines()
+    filepathname = os.path.join(filepath, "pdftron_wrap.cxx.replace")
+	with open(filepathname, "r") as f:
+	   cxx_replace = f.readlines()
 
-with open("pdftron_wrap.h.replace", "r") as f:
-   h_replace = f.readlines()
+    filepathname = os.path.join(filepath, "pdftron_wrap.h.replace")
+	with open(filepathname, "r") as f:
+	   h_replace = f.readlines()
 
-with open("pdftron.go.replace", "r") as f:
-   go_replace = f.readlines()
+    filepathname = os.path.join(filepath, "pdftron.go.replace")
+	with open(filepathname, "r") as f:
+	   go_replace = f.readlines()
 
-uid = re.search(r'(extern\s+\w+\s+_wrap_\w+_pdftron_)(\w+)(\()', go).group(2)
+	uid = re.search(r'(extern\s+\w+\s+_wrap_\w+_pdftron_)(\w+)(\()', go).group(2)
 
-go = execute_replace(go, go_replace)
-cxx = execute_replace(cxx, cxx_replace)
-h = execute_replace(h, h_replace)
+	go = execute_replace(go, go_replace)
+	cxx = execute_replace(cxx, cxx_replace)
+	h = execute_replace(h, h_replace)
 
-old_uid = '02581caacfa652f4'
-go = go.replace(old_uid, uid)
-cxx = cxx.replace(old_uid, uid)
-h = h.replace(old_uid, uid)
+	old_uid = '02581caacfa652f4'
+	go = go.replace(old_uid, uid)
+	cxx = cxx.replace(old_uid, uid)
+	h = h.replace(old_uid, uid)
 
-with open("pdftron_wrap.cxx", "w+") as f:
-   f.write(cxx)
+    filepathname = os.path.join(filepath, "pdftron_wrap.cxx")
+	with open(filepathname, "w+") as f:
+	   f.write(cxx)
 
-with open("pdftron_wrap.h", "w+") as f:
-   f.write(h)
+    filepathname = os.path.join(filepath, "pdftron_wrap.h")
+	with open(filepathname, "w+") as f:
+	   f.write(h)
 
-with open("pdftron.go", "w+") as f:
-   f.write(go)
+    filepathname = os.path.join(filepath, "pdftron.go")
+	with open(filepathname, "w+") as f:
+	   f.write(go)
