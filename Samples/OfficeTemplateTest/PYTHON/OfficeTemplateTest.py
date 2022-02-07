7cdabbf3e25a3022c7ca907d23268ab49e934e1d
Daniel Boerlage <dboerlage@pdftron.com>
2022-02-07 11:18:31 -0800

    Some work

---
+++ Samples/OfficeTemplateTest/PYTHON/OfficeTemplateTest.py
@@ -0,0 +1,72 @@
+#---------------------------------------------------------------------------------------
+# Copyright (c) 2001-2021 by PDFTron Systems Inc. All Rights Reserved.
+# Consult LICENSE.txt regarding license information.
+#---------------------------------------------------------------------------------------
+
+import site
+site.addsitedir("../../../PDFNetC/Lib")
+import sys
+from PDFNetPython import *
+
+sys.path.append("../../LicenseKey/PYTHON")
+from LicenseKey import *
+
+# Relative path to the folder containing the test files.
+input_path = "../../TestFiles/"
+output_path = "../../TestFiles/Output/"
+
+#------------------------------------------------------------------------------
+# The following sample illustrates how to use the PDF.Convert utility class
+# to convert MS Office files to PDF and replace templated tags present in the document
+# with content supplied via json
+#
+# For a detailed specification of the template format and supported features,
+# see: https://www.pdftron.com/documentation/core/guides/generate-via-template/data-model/
+#
+# This conversion is performed entirely within the PDFNet and has *no*
+# external or system dependencies -- Conversion results will be
+# the same whether on Windows, Linux or Android.
+#
+# Please contact us if you have any questions.
+#------------------------------------------------------------------------------
+
+def main():
+    # The first step in every application using PDFNet is to initialize the
+    # library. The library is usually initialized only once, but calling
+    # Initialize() multiple times is also fine.
+    PDFNet.Initialize(LicenseKey)
+    PDFNet.SetResourcesPath("../../../Resources")
+
+    input_filename = "SYH_Letter.docx"
+    output_filename = "SYH_Letter.pdf"
+
+    json = """
+    {{
+        "dest_given_name": "Janice N.",
+        "dest_street_address": "187 Duizelstraat",
+        "dest_surname": "Symonds",
+        "dest_title": "Ms.",
+        "land_location": "225 Parc St., Rochelle, QC ",
+        "lease_problem": "According to the city records, the lease was initiated in September 2010 and never terminated",
+        "logo": {{ "image_url": "{0}logo_red.png", "width" : 64, "height":  64 }},
+        "sender_name": "Arnold Smith"
+    }}
+    """.format(input_path)
+
+    # Start with a PDFDoc (the conversion destination)
+    pdfdoc = PDFDoc()
+
+    options = OfficeToPDFOptions()
+    options.SetTemplateParamsJson(json)
+
+	# perform the conversion with template delimiters and content dictionary
+    Convert.OfficeToPDF(pdfdoc, input_path + input_filename, options)
+
+    # save the result
+    pdfdoc.Save(output_path + output_filename, SDFDoc.e_linearized)
+
+    # And we're done!
+    print("Saved " + output_filename )
+
+if __name__ == '__main__':
+    main()
