29fb1a9e94b05b1b5a180735f9710cceaa7b6182
Daniel Boerlage <daniel.brlg@gmail.com>
2022-02-10 17:01:06 -0800

    Sample code OfficeTemplateTest (#51)
    *    Added OfficeTemplateTest sample
    *    Fixed bug in PDFNetPHP/CMakeLists where it was not properly detecting that php-config was not found

---
+++ Samples/OfficeTemplateTest/RUBY/OfficeTemplateTest.rb
@@ -0,0 +1,72 @@
+#---------------------------------------------------------------------------------------
+# Copyright (c) 2001-2021 by PDFTron Systems Inc. All Rights Reserved.
+# Consult LICENSE.txt regarding license information.
+#---------------------------------------------------------------------------------------
+
+require '../../../PDFNetC/Lib/PDFNetRuby'
+include PDFNetRuby
+require '../../LicenseKey/RUBY/LicenseKey'
+
+$stdout.sync = true
+
+#------------------------------------------------------------------------------
+# The following sample illustrates how to use the PDF.Convert utility class
+# to convert MS Office files to PDF and replace templated tags present in the document
+# with content supplied via json
+#
+# For a detailed specification of the template format and supported features,
+# see: https://www.pdftron.com/documentation/core/guides/generate-via-template/data-model/
+#
+# This conversion is performed entirely within the PDFNet and has *no*
+# external or system dependencies -- Conversion results will be
+# the same whether on Windows, Linux or Android.
+#
+# Please contact us if you have any questions.
+#------------------------------------------------------------------------------
+
+# Relative path to the folder containing the test files.
+$inputPath = "../../TestFiles/"
+$outputPath = "../../TestFiles/Output/"
+
+def main()
+    # The first step in every application using PDFNet is to initialize the
+    # library. The library is usually initialized only once, but calling
+    # Initialize() multiple times is also fine.
+    PDFNet.Initialize(PDFTronLicense.Key)
+    PDFNet.SetResourcesPath("../../../Resources")
+
+    inputFilename = "SYH_Letter.docx"
+    outputFilename = "SYH_Letter.pdf"
+
+    json = '
+    {
+        "dest_given_name": "Janice N.",
+        "dest_street_address": "187 Duizelstraat",
+        "dest_surname": "Symonds",
+        "dest_title": "Ms.",
+        "land_location": "225 Parc St., Rochelle, QC ",
+        "lease_problem": "According to the city records, the lease was initiated in September 2010 and never terminated",
+        "logo": { "image_url": "%slogo_red.png", "width" : 64, "height":  64 },
+        "sender_name": "Arnold Smith"
+    }
+    ' % inputPath
+
+    # Start with a PDFDoc (the conversion destination)
+    pdfdoc = PDFDoc.new()
+
+    options = OfficeToPDFOptions.new()
+    options.SetTemplateParamsJson(json)
+
+    # perform the conversion with template delimiters and content dictionary
+    inputFile = $inputPath + inputFilename
+    Convert.OfficeToPDF(pdfdoc, inputFile, options)
+
+    # save the result
+    outputFile = $outputPath + outputFilename
+    pdfdoc.Save(outputFile, SDFDoc::E_linearized)
+
+    # And we're done!
+    print("Saved " + outputFilename)
+end
+
+main()
