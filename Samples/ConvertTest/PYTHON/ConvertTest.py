9ecdb789bc02b9f2a7e193abb11881028fef7231
Igor Repinetski <irepinetski@pdftron.com>
2022-11-18 13:05:00 +0100

    PDFLIB-178: Python wrapper test updated also to run Convert::FromSVG()

--- Samples/ConvertTest/PYTHON/ConvertTest.py
+++ Samples/ConvertTest/PYTHON/ConvertTest.py
@@ -54,7 +54,8 @@ def ConvertToPdfFromFile():
     [ "simple-emf.emf","emf2pdf.pdf", True],
     [ "simple-xps.xps","xps2pdf.pdf", False],
     # [ "simple-webpage.mht","mht2pdf.pdf", True],
-    [ "simple-webpage.html","html2pdf.pdf", True]
+    [ "simple-webpage.html","html2pdf.pdf", True],
+    [ "tiger.svg", "svg2pdf.pdf", False]
     ]
     ret = 0
 
