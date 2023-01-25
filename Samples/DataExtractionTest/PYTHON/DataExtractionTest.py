ac87dc8e28979c2230d72d571c0b9f7104a4f72d
bcldemjen <tdemjen@pdftron.com>
2023-01-25 15:53:03 -0800

    CI-185 Data Extraction Module samples

---
+++ Samples/DataExtractionTest/PYTHON/DataExtractionTest.py
@@ -0,0 +1,166 @@
+#---------------------------------------------------------------------------------------
+# Copyright (c) 2001-2023 by PDFTron Systems Inc. All Rights Reserved.
+# Consult LICENSE.txt regarding license information.
+#---------------------------------------------------------------------------------------
+
+import site
+site.addsitedir("../../../PDFNetC/Lib")
+import sys
+from PDFNetPython import *
+
+import platform
+
+sys.path.append("../../LicenseKey/PYTHON")
+from LicenseKey import *
+
+#---------------------------------------------------------------------------------------
+# The Data Extraction suite is an optional PDFNet add-on collection that can be used to
+# extract various types of data from PDF documents.
+#
+# The PDFTron SDK Data Extraction suite can be downloaded from
+# https://www.pdftron.com/documentation/core/info/modules/
+#
+# Please contact us if you have any questions.
+#---------------------------------------------------------------------------------------
+
+# Relative path to the folder containing the test files.
+inputPath = "../../TestFiles/"
+outputPath = "../../TestFiles/Output/"
+
+def WriteTextToFile(output_path, text):
+    # Write the contents of the buffer to the disk
+    if sys.version_info.major >= 3:
+        f = open(output_path + "doc_memory_edit.txt", "w")
+    else:	
+        f = open(output_path + "doc_memory_edit.txt", "wb")
+    try:
+        f.write(text)
+    finally:
+        f.close()
+
+def main():
+    # The first step in every application using PDFNet is to initialize the 
+    # library. The library is usually initialized only once, but calling 
+    # Initialize() multiple times is also fine.
+    PDFNet.Initialize(LicenseKey)
+    
+    PDFNet.AddResourceSearchPath("../../../PDFNetC/Lib/")
+
+    #-----------------------------------------------------------------------------------
+    # The following sample illustrates how to extract tables from PDF documents.
+    #-----------------------------------------------------------------------------------
+
+    # Test if the add-on is installed
+    if not DataExtractionModule.IsModuleAvailable(DataExtractionModule.e_Tabular):
+        print("")
+        print("Unable to run Data Extraction: PDFTron SDK Tabular Data module not available.")
+        print("-----------------------------------------------------------------------------")
+        print("The Data Extraction suite is an optional add-on, available for download")
+        print("at https://www.pdftron.com/documentation/core/info/modules/. If you have already")
+        print("downloaded this module, ensure that the SDK is able to find the required files")
+        print("using the PDFNet.AddResourceSearchPath() function.")
+        print("")
+    else:
+        try:
+            # Extract tabular data as a JSON file
+            print("Extract tabular data as a JSON file")
+
+            outputFile = outputPath + "table.json"
+
+            json = DataExtractionModule.ExtractData(inputPath + "table.pdf", DataExtractionModule.e_Tabular)
+            WriteTextToFile(outputFile, json)
+
+            print("Result saved in " + outputFile)
+        except Exception as e:
+            print("Unable to extract tabular data, error: " + str(e))
+
+        try:
+            # Extract tabular data as an XLSX file
+            print("Extract tabular data as an XLSX file")
+
+            outputFile = outputPath + "table.xlsx"
+            DataExtractionModule.ExtractToXSLX(inputPath + "table.pdf", outputFile)
+
+            print("Result saved in " + outputFile)
+        except Exception as e:
+            print("Unable to extract tabular data, error: " + str(e))
+
+        try:
+            # Extract tabular data as an XLSX stream (also known as filter)
+            print("Extract tabular data as an XLSX stream")
+
+            outputFile = outputPath + "table_streamed.xlsx"
+            options = DataExtractionOptions()
+            options.SetPages("1"); # page 1
+            outputXlsxStream = Filters.MemoryFilter(0, False)
+            DataExtractionModule.ExtractToXSLX(inputPath + "table.pdf", outputXlsxStream, options)
+            outputXlsxStream.SetAsInputFilter()
+            outputXlsxStream.WriteToFile(outputFile, False)
+
+            print("Result saved in " + outputFile)
+        except Exception as e:
+            print("Unable to extract tabular data, error: " + str(e))
+
+    #-----------------------------------------------------------------------------------
+    # The following sample illustrates how to extract document structure from PDF documents.
+    #-----------------------------------------------------------------------------------
+
+    # Test if the add-on is installed
+    if not DataExtractionModule.IsModuleAvailable(DataExtractionModule.e_DocStructure):
+        print("")
+        print("Unable to run Data Extraction: PDFTron SDK Structured Output module not available.")
+        print("-----------------------------------------------------------------------------")
+        print("The Data Extraction suite is an optional add-on, available for download")
+        print("at https://www.pdftron.com/documentation/core/info/modules/. If you have already")
+        print("downloaded this module, ensure that the SDK is able to find the required files")
+        print("using the PDFNet.AddResourceSearchPath() function.")
+        print("")
+    else:
+        try:
+            # Extract document structure as a JSON file
+            print("Extract document structure as a JSON file")
+
+            outputFile = outputPath + "paragraphs_and_tables.json"
+
+            json = DataExtractionModule.ExtractData(inputPath + "paragraphs_and_tables.pdf", DataExtractionModule.e_DocStructure)
+            WriteTextToFile(outputFile, json)
+
+            print("Result saved in " + outputFile)
+        except Exception as e:
+            print("Unable to extract document structure data, error: " + str(e))
+
+    #-----------------------------------------------------------------------------------
+    # The following sample illustrates how to extract form fields from PDF documents.
+    #-----------------------------------------------------------------------------------
+
+    # Test if the add-on is installed
+    if not DataExtractionModule.IsModuleAvailable(DataExtractionModule.e_Form):
+        print("")
+        print("Unable to run Data Extraction: PDFTron SDK AIFormFieldExtractor module not available.")
+        print("-----------------------------------------------------------------------------")
+        print("The Data Extraction suite is an optional add-on, available for download")
+        print("at https://www.pdftron.com/documentation/core/info/modules/. If you have already")
+        print("downloaded this module, ensure that the SDK is able to find the required files")
+        print("using the PDFNet.AddResourceSearchPath() function.")
+        print("")
+    else:
+        try:
+            # Extract form fields as a JSON file
+            print("Extract form fields as a JSON file")
+
+            outputFile = outputPath + "formfield.json"
+
+            json = DataExtractionModule.ExtractData(inputPath + "formfield.pdf", DataExtractionModule.e_Form)
+            WriteTextToFile(outputFile, json)
+
+            print("Result saved in " + outputFile)
+        except Exception as e:
+            print("Unable to extract form fields data, error: " + str(e))
+
+    #-----------------------------------------------------------------------------------
+
+    PDFNet.Terminate()
+    print("Done.")
+    
+if __name__ == '__main__':
+    main()
