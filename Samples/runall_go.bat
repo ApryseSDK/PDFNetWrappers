2ca0847bc376f79ab6a65c2da7653c4ccb325835
Ramzi <rchennafi@pdftron.com>
2023-03-01 17:07:28 -0800

    Fixed all the tests for go test.
    Handful didn't compile properly.

--- Samples/runall_go.bat
+++ Samples/runall_go.bat
@@ -1,1 +1,9 @@
-go test
+LICENSE_KEY=""
+
+if exist go.mod (
+    go mod init pdftron-test
+    go mod edit -replace github.com/pdftron/pdftron-go=../
+    go mod tidy
+)
+
+go test ./... -license=%LICENSE_KEY%
