e8b7d8a3ecf605f45e37bca7a4f57dd6aa4b80c8
Mitch (Minh) Nguyen <mnguyen@pdftron.com>
2023-05-16 14:26:56 -0700

    Split ConvertTest

--- Samples/ConvertTest/PYTHON/ConvertTest.py
+++ Samples/ConvertTest/PYTHON/ConvertTest.py
@@ -14,23 +14,12 @@ sys.path.append("../../LicenseKey/PYTHON")
 from LicenseKey import *
 
 #---------------------------------------------------------------------------------------
-# The following sample illustrates how to use the PDF.Convert utility class to convert 
-# documents and files to PDF, XPS, SVG, or EMF.
+# The following sample illustrates how to use the PDF::Convert utility class to convert 
+# documents and files to PDF, XPS, or SVG, or EMF. The sample also shows how to convert MS Office files 
+# using our built in conversion.
 #
 # Certain file formats such as XPS, EMF, PDF, and raster image formats can be directly 
-# converted to PDF or XPS. Other formats are converted using a virtual driver. To check 
-# if ToPDF (or ToXPS) require that PDFNet printer is installed use Convert.RequiresPrinter(filename). 
-# The installing application must be run as administrator. The manifest for this sample 
-# specifies appropriate the UAC elevation.
-#
-# Note: the PDFNet printer is a virtual XPS printer supported on Vista SP1 and Windows 7.
-# For Windows XP SP2 or higher, or Vista SP0 you need to install the XPS Essentials Pack (or 
-# equivalent redistributables). You can download the XPS Essentials Pack from:
-#        http:#www.microsoft.com/downloads/details.aspx?FamilyId=B8DCFFDD-E3A5-44CC-8021-7649FD37FFEE&displaylang=en
-# Windows XP Sp2 will also need the Microsoft Core XML Services (MSXML) 6.0:
-#         http:#www.microsoft.com/downloads/details.aspx?familyid=993C0BCF-3BCF-4009-BE21-27E85E1857B1&displaylang=en
-#
-# Note: Convert.fromEmf and Convert.toEmf will only work on Windows and require GDI+.
+# converted to PDF or XPS. 
 #
 # Please contact us if you have any questions.    
 #---------------------------------------------------------------------------------------
@@ -40,57 +29,6 @@ inputPath = "../../TestFiles/"
 outputPath = "../../TestFiles/Output/"
 
 
-# convert from a file to PDF automatically
-def ConvertToPdfFromFile():
-    testfiles = [
-    [ "simple-word_2007.docx","docx2pdf.pdf", False],
-    [ "simple-powerpoint_2007.pptx","pptx2pdf.pdf", False],
-    [ "simple-excel_2007.xlsx","xlsx2pdf.pdf", False],
-    [ "simple-publisher.pub","pub2pdf.pdf", True],
-    # [ "simple-visio.vsd","vsd2pdf.pdf"],# requires Microsoft Office Visio
-    [ "simple-text.txt","txt2pdf.pdf", False],
-    [ "simple-rtf.rtf","rtf2pdf.pdf", True],
-    [ "butterfly.png","png2pdf.pdf", False],
-    [ "simple-emf.emf","emf2pdf.pdf", True],
-    [ "simple-xps.xps","xps2pdf.pdf", False],
-    # [ "simple-webpage.mht","mht2pdf.pdf", True],
-    [ "simple-webpage.html","html2pdf.pdf", True]
-    ]
-    ret = 0
-
-    if platform.system() == 'Windows':
-        try:
-            if ConvertPrinter.IsInstalled("PDFTron PDFNet"):
-                ConvertPrinter.SetPrinterName("PDFTron PDFNet")
-            elif not ConvertPrinter.isInstalled():
-                try:
-                    print("Installing printer (requires Windows platform and administrator)")
-                    ConvertPrinter.Install()
-                    print("Installed printer " + ConvertPrinter.getPrinterName())
-                    # the function ConvertToXpsFromFile may require the printer so leave it installed
-                    # uninstallPrinterWhenDone = true;
-                except:
-                    print("ERROR: Unable to install printer.")
-        except:
-            print("ERROR: Unable to install printer.")
-
-    for testfile in testfiles:
-        if platform.system() != 'Windows':
-            if testfile[2]:
-                continue
-        try:
-            pdfdoc = PDFDoc()
-            inputFile = testfile[0]
-            outputFile = testfile[1]
-            if Convert.RequiresPrinter(inputPath + inputFile):
-                print("Using PDFNet printer to convert file " + inputFile)
-            Convert.ToPdf(pdfdoc, inputPath + inputFile)
-            pdfdoc.Save(outputPath + outputFile, SDFDoc.e_compatibility)
-            pdfdoc.Close()
-            print("Converted file: " + inputFile + "\nto: " + outputFile)
-        except:
-            ret = 1
-    return ret
 
 def ConvertSpecificFormats():
     ret = 0
@@ -106,14 +44,6 @@ def ConvertSpecificFormats():
         pdfdoc.Save(outputPath + outputFile, SDFDoc.e_remove_unused)
         print("Saved " + outputFile)
         
-        # Convert the EMF document to PDF
-        if platform.system() == 'Windows':
-            s1 = inputPath + "simple-emf.emf"
-            print("Converting from EMF")
-            Convert.FromEmf(pdfdoc, s1)
-            outputFile = "emf2pdf v2.pdf"
-            pdfdoc.Save(outputPath + outputFile, SDFDoc.e_remove_unused)
-            print("Saved " + outputFile)
 
         # Convert the TXT document to PDF
         set =  ObjSet()
@@ -179,6 +109,37 @@ def ConvertSpecificFormats():
     except:
         ret = 1
     return ret
+
+# convert from a file to PDF automatically
+def ConvertToPdfFromFile():
+    testfiles = [
+    [ "simple-word_2007.docx","docx2pdf.pdf"],
+    [ "simple-powerpoint_2007.pptx","pptx2pdf.pdf"],
+    [ "simple-excel_2007.xlsx","xlsx2pdf.pdf"],
+    [ "simple-text.txt","txt2pdf.pdf"],
+    [ "butterfly.png","png2pdf.pdf"],
+    [ "simple-xps.xps","xps2pdf.pdf"],
+    ]
+    ret = 0
+
+
+    for testfile in testfiles:
+        try:
+            pdfdoc = PDFDoc()
+            inputFile = testfile[0]
+            outputFile = testfile[1]
+            Printer.SetMode(Printer.e_prefer_builtin_converter)
+
+            Convert.ToPdf(pdfdoc, inputPath + inputFile)
+            pdfdoc.Save(outputPath + outputFile, SDFDoc.e_linearized)
+            pdfdoc.Close()
+            print("Converted file: " + inputFile + "\nto: " + outputFile)
+        except:
+            ret = 1
+            print("ERROR: on input file " + inputFile)
+    return ret
+
+
 def main():
     # The first step in every application using PDFNet is to initialize the 
     # library. The library is usually initialized only once, but calling 
@@ -199,13 +160,6 @@ def main():
     else:
         print("ConvertSpecificFormats succeeded")
 
-    if platform.system() == 'Windows':
-        try:
-            print("Uninstalling printer (requires Windows platform and administrator)")
-            ConvertPrinter.Uninstall()
-            print("Uninstalled printer " + ConvertPrinter.getPrinterName())
-        except:
-            print("Unable to uninstall printer")
 
     PDFNet.Terminate()
     print("Done.")
