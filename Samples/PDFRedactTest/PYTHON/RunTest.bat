2dfbbea56886bb52738028a31182af75d81035a1
Ramzi Chennafi <rchennafi@apryse.com>
2025-05-15 12:29:34 -0700

    Change way we run scripts.

--- Samples/PDFRedactTest/PYTHON/RunTest.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-call ..\..\py_init.bat
-if %ERRORLEVEL% NEQ 0 goto EOF
-set TEST_NAME=PDFRedactTest
-python.exe -u %TEST_NAME%.py
-:EOF
-endlocal
--- Samples/SDFTest/PYTHON/RunTest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-TEST_NAME=SDFTest
-export LD_LIBRARY_PATH=../../../PDFNetC/Lib
-. ../../py_init.sh
-$python_exe -u $TEST_NAME.py
