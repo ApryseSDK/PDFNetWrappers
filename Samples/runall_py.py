72e6d0275e294b1f6ff7500b4bcbae125c9f5e6e
Ramzi <rchennafi@apryse.com>
2025-06-05 22:53:55 -0700

    Fix libinfo sample script.

--- Samples/runall_py.py
+++ Samples/runall_py.py
@@ -6,8 +6,10 @@ print("Running Python tests with %s", sys.executable)
 
 for name in os.listdir('./'):
     if os.path.isdir(name):
-        ret = subprocess.run([sys.executable, "%s/PYTHON/%s.py" % (name, name)])
-        if ret != 0:
-            print("Test %s failed!" % name)
+        items = os.listdir(name)
+        if "PYTHON" in items:
+            ret = subprocess.run([sys.executable, "%s.py" % name], cwd="%s/PYTHON" % name)
+            if ret != 0:
+                print("Test %s failed!" % name)
 
 print("Tests completed");
