493b14a692c0a48f7ded3ef454faec200c440152
rogerk-apryse <roger.khoueiri@apryse.com>
2026-01-15 17:09:25 -0500

    CAP-1609 Fixing failed Go samples
    - Fixes for the OCRTest samples to work with the IRIS OCR module if present. Before the change, if the module is present, and we are not requesting the use of that module, it was giving an error.
    - Removed test case number 7 from these tests as it was previously removed from the CPP and Java tests but not from these. Test 7 is expected to fail.

--- Samples/OCRTest/GO/OCR_test.go
+++ Samples/OCRTest/GO/OCR_test.go
@@ -5,18 +5,18 @@
 
 package main
 import (
-        "fmt"
-        "testing"
-        "flag"
-        . "github.com/pdftron/pdftron-go/v2"
+	"fmt"
+	"testing"
+    "flag"
+	. "github.com/pdftron/pdftron-go/v2"
 )
 
 var licenseKey string
 var modulePath string
 
 func init() {
-    flag.StringVar(&licenseKey, "license", "", "License key for Apryse SDK")
-    flag.StringVar(&modulePath, "modulePath", "", "Path for downloaded modules")
+	flag.StringVar(&licenseKey, "license", "", "License key for Apryse SDK")
+	flag.StringVar(&modulePath, "modulePath", "", "Path for downloaded modules")
 }
 
 // Relative path to the folder containing test files.
@@ -27,217 +27,212 @@ var outputPath = "../TestFiles/Output/"
 // The following sample illustrates how to use OCR module
 // --------------------------------------------------------------------------------------
 
-func TestOCR(t *testing.T){
+func TestOCR(t *testing.T) {
 
-    // The first step in every application using PDFNet is to initialize the
-    // library and set the path to common PDF resources. The library is usually
-    // initialized only once, but calling Initialize() multiple times is also fine.
-    PDFNetInitialize(licenseKey)
+	// The first step in every application using PDFNet is to initialize the
+	// library and set the path to common PDF resources. The library is usually
+	// initialized only once, but calling Initialize() multiple times is also fine.
+	PDFNetInitialize(licenseKey)
 
-    // The location of the OCR Module
-    PDFNetAddResourceSearchPath(modulePath);
+	// The location of the OCR Module
+	PDFNetAddResourceSearchPath(modulePath)
 
-    if ! OCRModuleIsModuleAvailable(){
+	// if the IRIS OCR module is available, will use that instead of the default
+	use_iris := OCRModuleIsIRISModuleAvailable()
+	if !OCRModuleIsModuleAvailable() {
 
-        fmt.Println("Unable to run OCRTest: PDFTron SDK OCR module not available.\n" +
-        "---------------------------------------------------------------\n" +
-        "The OCR module is an optional add-on, available for download\n" +
-        "at https://dev.apryse.com/. If you have already downloaded this\n" +
-        "module, ensure that the SDK is able to find the required files\n" +
-        "using the PDFNet::AddResourceSearchPath() function.")
+		fmt.Println("Unable to run OCRTest: PDFTron SDK OCR module not available.\n" +
+			"---------------------------------------------------------------\n" +
+			"The OCR module is an optional add-on, available for download\n" +
+			"at https://dev.apryse.com/. If you have already downloaded this\n" +
+			"module, ensure that the SDK is able to find the required files\n" +
+			"using the PDFNet::AddResourceSearchPath() function.")
 
-    }else{
+	} else {
 
-        // Example 1) Process image without specifying options, default language - English - is used
-        // --------------------------------------------------------------------------------
+		// Example 1) Process image without specifying options, default language - English - is used
+		// --------------------------------------------------------------------------------
 
-        // A) Setup empty destination doc
+		// A) Setup empty destination doc
 
-        doc := NewPDFDoc()
+		doc := NewPDFDoc()
 
-        // B) Run OCR on the .png with options
-        
-        ocrOpts := NewOCROptions()
-        OCRModuleImageToPDF(doc, inputPath + "psychomachia_excerpt.png", ocrOpts)
+		// B) Run OCR on the .png with options
 
-        // C) Check the result
+		opts := NewOCROptions()
+		if use_iris {
+			opts.SetOCREngine("iris")
+		}
 
-        doc.Save(outputPath + "psychomachia_excerpt.pdf", uint(0))
-        fmt.Println("Example 1: psychomachia_excerpt.png")
+		OCRModuleImageToPDF(doc, inputPath+"psychomachia_excerpt.png", opts)
 
-        // Example 2) Process document using multiple languages
-        // --------------------------------------------------------------------------------
+		// C) Check the result
 
-        // A) Setup empty destination doc
+		doc.Save(outputPath+"psychomachia_excerpt.pdf", uint(0))
+		fmt.Println("Example 1: psychomachia_excerpt.png")
 
-        doc = NewPDFDoc()
+		// Example 2) Process document using multiple languages
+		// --------------------------------------------------------------------------------
 
-        // B) Setup options with multiple target languages, English will always be considered as secondary language
+		// A) Setup empty destination doc
 
-        opts := NewOCROptions()
-        opts.AddLang("deu")
-        opts.AddLang("fra")
-        opts.AddLang("eng")
+		doc = NewPDFDoc()
 
-        // C) Run OCR on the .jpg with options
+		// B) Setup options with multiple target languages, English will always be considered as secondary language
 
-        OCRModuleImageToPDF(doc, inputPath + "multi_lang.jpg", opts)
+		opts = NewOCROptions()
+		if use_iris {
+			opts.SetOCREngine("iris")
+		}
+		opts.AddLang("deu")
+		opts.AddLang("fra")
+		opts.AddLang("eng")
 
-        // D) Check the result
+		// C) Run OCR on the .jpg with options
 
-        doc.Save(outputPath + "multi_lang.pdf", uint(0))
-        fmt.Println("Example 2: multi_lang.jpg")
+		OCRModuleImageToPDF(doc, inputPath+"multi_lang.jpg", opts)
 
-        // Example 3) Process a .pdf specifying a language - German - and ignore zone comprising a sidebar image
-        // --------------------------------------------------------------------------------
+		// D) Check the result
 
-        // A) Open the .pdf document
+		doc.Save(outputPath+"multi_lang.pdf", uint(0))
+		fmt.Println("Example 2: multi_lang.jpg")
 
-        doc = NewPDFDoc(inputPath + "german_kids_song.pdf")
+		// Example 3) Process a .pdf specifying a language - German - and ignore zone comprising a sidebar image
+		// --------------------------------------------------------------------------------
 
-        // B) Setup options with a single language and an ignore zone
+		// A) Open the .pdf document
 
-        opts = NewOCROptions()
-        opts.AddLang("deu")
+		doc = NewPDFDoc(inputPath + "german_kids_song.pdf")
 
-        ignoreZones := NewRectCollection()
-        ignoreZones.AddRect(NewRect(424.0, 163.0, 493.0, 730.0))
-        opts.AddIgnoreZonesForPage(ignoreZones, 1)
+		// B) Setup options with a single language and an ignore zone
 
-        // C) Run OCR on the .pdf with options
+		opts = NewOCROptions()
+		if use_iris {
+			opts.SetOCREngine("iris")
+		}
+		opts.AddLang("deu")
 
-        OCRModuleProcessPDF(doc, opts)
+		ignoreZones := NewRectCollection()
+		ignoreZones.AddRect(NewRect(424.0, 163.0, 493.0, 730.0))
+		opts.AddIgnoreZonesForPage(ignoreZones, 1)
 
-        // D) check the result
+		// C) Run OCR on the .pdf with options
 
-        doc.Save(outputPath + "german_kids_song.pdf", uint(0))
-        fmt.Println("Example 3: german_kids_song.pdf")
+		OCRModuleProcessPDF(doc, opts)
 
-        // Example 4) Process multi-page tiff with text/ignore zones specified for each page,
-        // optionally provide English as the target language
-        // --------------------------------------------------------------------------------
+		// D) check the result
 
-        // A) Setup empty destination doc
+		doc.Save(outputPath+"german_kids_song.pdf", uint(0))
+		fmt.Println("Example 3: german_kids_song.pdf")
 
-        doc = NewPDFDoc()
+		// Example 4) Process multi-page tiff with text/ignore zones specified for each page,
+		// optionally provide English as the target language
+		// --------------------------------------------------------------------------------
 
-        // B) Setup options with a single language plus text/ignore zones
+		// A) Setup empty destination doc
 
-        opts = NewOCROptions()
-        opts.AddLang("eng")
+		doc = NewPDFDoc()
 
-        ignoreZones = NewRectCollection()
+		// B) Setup options with a single language plus text/ignore zones
 
-        // ignore signature box in the first 2 pages
-        ignoreZones.AddRect(NewRect(1492.0, 56.0, 2236.0, 432.0))
-        opts.AddIgnoreZonesForPage(ignoreZones, 1)
+		opts = NewOCROptions()
+		if use_iris {
+			opts.SetOCREngine("iris")
+		}
+		opts.AddLang("eng")
 
-        opts.AddIgnoreZonesForPage(ignoreZones, 2)
+		ignoreZones = NewRectCollection()
 
-        // can use a combination of ignore and text boxes to focus on the page area of interest,
-        // as ignore boxes are applied first, we remove the arrows before selecting part of the diagram
-        ignoreZones.Clear()
-        ignoreZones.AddRect(NewRect(992.0, 1276.0, 1368.0, 1372.0))
-        opts.AddIgnoreZonesForPage(ignoreZones, 3)
+		// ignore signature box in the first 2 pages
+		ignoreZones.AddRect(NewRect(1492.0, 56.0, 2236.0, 432.0))
+		opts.AddIgnoreZonesForPage(ignoreZones, 1)
 
-        textZones := NewRectCollection()
-        // we only have text zones selected in page 3
+		opts.AddIgnoreZonesForPage(ignoreZones, 2)
 
-        // select horizontal BUFFER ZONE sign
-        textZones.AddRect(NewRect(900.0, 2384.0, 1236.0, 2480.0))
+		// can use a combination of ignore and text boxes to focus on the page area of interest,
+		// as ignore boxes are applied first, we remove the arrows before selecting part of the diagram
+		ignoreZones.Clear()
+		ignoreZones.AddRect(NewRect(992.0, 1276.0, 1368.0, 1372.0))
+		opts.AddIgnoreZonesForPage(ignoreZones, 3)
 
-        // select right vertical BUFFER ZONE sign
-        textZones.AddRect(NewRect(1960.0, 1976.0, 2016.0, 2296.0))
-        // select Lot No.
-        textZones.AddRect(NewRect(696.0, 1028.0, 1196.0, 1128.0))
+		textZones := NewRectCollection()
+		// we only have text zones selected in page 3
 
-        // select part of the plan inside the BUFFER ZONE
-        textZones.AddRect(NewRect(428.0, 1484.0, 1784.0, 2344.0))
-        textZones.AddRect(NewRect(948.0, 1288.0, 1672.0, 1476.0))
-        opts.AddTextZonesForPage(textZones, 3)
+		// select horizontal BUFFER ZONE sign
+		textZones.AddRect(NewRect(900.0, 2384.0, 1236.0, 2480.0))
 
-        // C) Run OCR on the .pdf with options
+		// select right vertical BUFFER ZONE sign
+		textZones.AddRect(NewRect(1960.0, 1976.0, 2016.0, 2296.0))
+		// select Lot No.
+		textZones.AddRect(NewRect(696.0, 1028.0, 1196.0, 1128.0))
 
-        OCRModuleImageToPDF(doc, inputPath + "bc_environment_protection.tif", opts)
+		// select part of the plan inside the BUFFER ZONE
+		textZones.AddRect(NewRect(428.0, 1484.0, 1784.0, 2344.0))
+		textZones.AddRect(NewRect(948.0, 1288.0, 1672.0, 1476.0))
+		opts.AddTextZonesForPage(textZones, 3)
 
-        // D) check the result
+		// C) Run OCR on the .pdf with options
 
-        doc.Save(outputPath + "bc_environment_protection.pdf", uint(0))
-        fmt.Println("Example 4: bc_environment_protection.tif")
+		OCRModuleImageToPDF(doc, inputPath+"bc_environment_protection.tif", opts)
 
-        // Example 5) Alternative workflow for extracting OCR result JSON, postprocessing
-        // (e.g., removing words not in the dictionary or filtering special
-        // out special characters), and finally applying modified OCR JSON to the source PDF document
-        // --------------------------------------------------------------------------------
+		// D) check the result
 
-        // A) Open the .pdf document
+		doc.Save(outputPath+"bc_environment_protection.pdf", uint(0))
+		fmt.Println("Example 4: bc_environment_protection.tif")
 
-        doc = NewPDFDoc(inputPath + "zero_value_test_no_text.pdf")
+		// Example 5) Alternative workflow for extracting OCR result JSON, postprocessing
+		// (e.g., removing words not in the dictionary or filtering special
+		// out special characters), and finally applying modified OCR JSON to the source PDF document
+		// --------------------------------------------------------------------------------
 
-        // B) Run OCR on the .pdf with default English language
+		// A) Open the .pdf document
 
-        opts = NewOCROptions()
-        json := OCRModuleGetOCRJsonFromPDF(doc, opts)
+		doc = NewPDFDoc(inputPath + "zero_value_test_no_text.pdf")
 
-        // C) Post-processing step (whatever it might be)
+		// B) Run OCR on the .pdf with default English language
 
-        fmt.Println("Have OCR result JSON, re-applying to PDF")
+		opts = NewOCROptions()
+		if use_iris {
+			opts.SetOCREngine("iris")
+		}
+		json := OCRModuleGetOCRJsonFromPDF(doc, opts)
 
-        OCRModuleApplyOCRJsonToPDF(doc, json)
+		// C) Post-processing step (whatever it might be)
 
-        // D) Check the result
+		fmt.Println("Have OCR result JSON, re-applying to PDF")
 
-        doc.Save(outputPath + "zero_value_test_no_text.pdf", uint(0))
-        fmt.Println("Example 5: extracting and applying OCR JSON from zero_value_test_no_text.pdf")
+		OCRModuleApplyOCRJsonToPDF(doc, json)
 
-        // Example 6) The postprocessing workflow has also an option of extracting OCR results in XML format,
-        // similar to the one used by TextExtractor
-        // --------------------------------------------------------------------------------
+		// D) Check the result
 
-        // A) Setup empty destination doc
+		doc.Save(outputPath+"zero_value_test_no_text.pdf", uint(0))
+		fmt.Println("Example 5: extracting and applying OCR JSON from zero_value_test_no_text.pdf")
 
-        doc = NewPDFDoc()
+		// Example 6) The postprocessing workflow has also an option of extracting OCR results in XML format,
+		// similar to the one used by TextExtractor
+		// --------------------------------------------------------------------------------
 
-        // B) Run OCR on the .tif with default English language, extracting OCR results in XML format. Note that
-        // in the process we convert the source image into PDF.
-        // We reuse this PDF document later to add hidden text layer to it.
+		// A) Setup empty destination doc
 
-        xml := OCRModuleGetOCRXmlFromImage(doc, inputPath + "physics.tif", opts)
+		doc = NewPDFDoc()
 
-        // C) Post-processing step (whatever it might be)
+		// B) Run OCR on the .tif with default English language, extracting OCR results in XML format. Note that
+		// in the process we convert the source image into PDF.
+		// We reuse this PDF document later to add hidden text layer to it.
 
-        fmt.Println("Have OCR result XML, re-applying to PDF")
+		xml := OCRModuleGetOCRXmlFromImage(doc, inputPath+"physics.tif", opts)
 
-        OCRModuleApplyOCRXmlToPDF(doc, xml)
+		// C) Post-processing step (whatever it might be)
 
-        // D) Check the result
+		fmt.Println("Have OCR result XML, re-applying to PDF")
 
-        doc.Save(outputPath + "physics.pdf", uint(0))
-        fmt.Println("Example 6: extracting and applying OCR XML from physics.tif")
+		OCRModuleApplyOCRXmlToPDF(doc, xml)
 
-        // Example 7) Resolution can be manually set, when DPI missing from metadata or is wrong
-        // --------------------------------------------------------------------------------
+		// D) Check the result
 
-        // A) Setup empty destination doc
+		doc.Save(outputPath+"physics.pdf", uint(0))
+		fmt.Println("Example 6: extracting and applying OCR XML from physics.tif")
 
-        doc = NewPDFDoc()
-
-        // B) Setup options with a text zone
-
-        opts = NewOCROptions()
-        textZones = NewRectCollection()
-        textZones.AddRect(NewRect(140.0, 870.0, 310.0, 920.0))
-        opts.AddTextZonesForPage(textZones, 1)
-
-        // C) Manually override DPI
-        opts.AddDPI(100)
-
-        // D) Run OCR on the .jpg with options
-        OCRModuleImageToPDF(doc, inputPath + "corrupted_dpi.jpg", opts)
-
-        // E) Check the result
-        doc.Save(outputPath + "corrupted_dpi.pdf", uint(0))
-        PDFNetTerminate()
-        fmt.Println("Example 7: converting image with corrupted resolution metadata corrupted_dpi.jpg to pdf with searchable text")
-    }
+		PDFNetTerminate()
+	}
 }
